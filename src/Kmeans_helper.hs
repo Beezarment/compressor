7d7844a6821a5caeb6887f2318997017a152c8d2
Beezarment <armand.dufresne@epitech.eu>
2025-03-24 15:15:37 +0100

    [ADD] Kmeans calculus functions

--- src/Kmeans_helper.hs
+++ src/Kmeans_helper.hs
@@ -1,2 +1,20 @@
+{-
+-- EPITECH PROJECT, 2025
+-- Image_compressor
+-- File description:
+-- Kmeans_helper
+-}
 
-module Kmeans_helper where
+module Kmeans_helper (euclideanDistance, computeCentroid) where
+
+
+euclideanDistance :: (Int, Int, Int) -> (Int, Int, Int) -> Double
+euclideanDistance (r1, g1, b1) (r2, g2, b2) =
+    sqrt (fromIntegral ((r1 - r2)^2 + (g1 - g2)^2 + (b1 - b2)^2))
+
+computeCentroid :: [(Int, Int, Int)] -> (Int, Int, Int)
+computeCentroid colors =
+    let (rSum, gSum, bSum, count) = foldl (\(r, g, b, n) (r', g', b') ->
+                                            (r + r', g + g', b + b', n + 1))
+                                            (0, 0, 0, 0) colors
+    in (rSum `div` count, gSum `div` count, bSum `div` count)
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,18 @@
+{-
+-- EPITECH PROJECT, 2025
+-- Image_compressor
+-- File description:
+-- Main
+-}
+
 module Main where
 
-import Kmeans()
-import Kmeans_helper()
-import Parser()
+import System.Environment (getArgs)
+import Parser (parseLine)
+import Kmeans (kmeans)
 
 main :: IO ()
 main = putStrLn "Image Compressor Running..."
+
+errorExit :: IO ()
+errorExit = ioError (userError "Exit with error code 84")
